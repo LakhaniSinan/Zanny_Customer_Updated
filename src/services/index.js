ab3f084e22cc80bd8cc2a07a2645890ab4fc8778
LakhaniSinan <sinan.lakhani09@gmail.com>
2025-12-22 17:47:51 +0500

    updates

--- src/services/index.js
+++ src/services/index.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 // let baseUrl = 'http://192.168.0.62:4000/api/';
 // let baseUrl = 'http://192.168.156.54:4000/api/';
 // let baseUrl = 'https://czmgcw3c-4000.inc1.devtunnels.ms/api/';
-// let baseUrl = 'https://sirldigital.com/zannyFoods/api/';
+let baseUrl = 'https://sirldigital.com/zannyFoods/api/';
 // let baseUrl = 'https://0g01d8wd-4000.inc1.devtunnels.ms/api/';
 // let baseUrl = 'https://2mxcskzp-4000.inc1.devtunnels.ms/api/';
-let baseUrl = 'https://tk4c2l16-4000.euw.devtunnels.ms/api/';
+// let baseUrl = 'https://tk4c2l16-4000.euw.devtunnels.ms/api/';
 
 const api = async (path, params, method) => {
   let options;
